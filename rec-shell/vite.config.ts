5cd5b49533c0d44c63f10b5ab6abae8f2832a443
Kevin Bastidas <kevin-10b2011@hotmail.com>
2025-09-13 21:57:20 -0500

    Update vite.config.ts

--- rec-shell/vite.config.ts
+++ rec-shell/vite.config.ts
@@ -1,42 +1,47 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig(() => ({
+export default defineConfig({
   root: __dirname,
-  cacheDir: '../node_modules/.vite/rec-shell',
+  cacheDir: '../../node_modules/.vite/apps/rec-shell',
+  
+  // Cambiar a ruta relativa para que use ./assets/
+  base: './',
+  
   server: {
     port: 4200,
     host: 'localhost',
   },
+
   preview: {
-    port: 4200,
+    port: 4300,
     host: 'localhost',
   },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+
+  plugins: [react(), nxViteTsPaths()],
+
   build: {
-    outDir: '../dist/rec-shell',
+    outDir: '../../dist/apps/rec-shell',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    assetsDir: 'assets',
   },
+
   test: {
-    watch: false,
     globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
     environment: 'jsdom',
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../coverage/rec-shell',
-      provider: 'v8' as const,
+      reportsDirectory: '../../coverage/apps/rec-shell',
+      provider: 'v8',
     },
   },
-}));
+});
